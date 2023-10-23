3dcfd178e295916f5a422541f93be9ff87517890
Ignazio Neto Dell'Acqua <ignaiio123@gmail.com>
2023-10-23 19:52:05 +0200

    HW_5
    project a completed and correctly work . Project b doesnt work but the idea seems correct

--- HW_04/HW_04_2c/Core/Src/main.c
+++ HW_04/HW_04_2c/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
+#include "PMDB16_LCD.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
